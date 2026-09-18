4f2bb230ba8f9c82909fe87b838620808586055b
Urjit Mehta <154580093+UrjitMehta@users.noreply.github.com>
2026-09-18 15:18:57 +0400

    Add global seed function and directory setup
    Added a function to set a global seed for reproducibility and created necessary directories for model output and logs.

--- src/imports.py
+++ src/imports.py
@@ -44,3 +44,48 @@ from skimage.morphology import opening, closing, footprint_rectangle
 
 # Enable mixed-precision training for improved computational efficiency.
 mixed_precision.set_global_policy("mixed_float16")
+
+
+def set_global_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+
+
+set_global_seed(42)
+
+
+# ========== CONFIG PATHS ==========
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+dataset_dir = os.path.join(project_root, "dataset", "combined_dataset")
+
+train_img_dir = os.path.join(dataset_dir, "train_images")
+train_lbl_dir = os.path.join(dataset_dir, "train_all_TIFF_labels")
+# train_lbl_dir = os.path.join(dataset_dir, "train_labels")
+
+test_img_dir = os.path.join(dataset_dir, "test_images")
+test_lbl_dir = os.path.join(dataset_dir, "test_all_TIFF_labels")
+# test_lbl_dir = os.path.join(dataset_dir, "test_labels")
+
+model_dir = os.path.join(project_root, "model_output", "model", "swin_cl_200_test")
+loss_dir = os.path.join(project_root, "model_output", "loss_output", "swin_cl_200_test")
+output_dir = os.path.join(project_root, "model_output", "epoch_output", "swin_cl_200_test")
+
+log_dir = os.path.join(project_root, "model_output", "logs", "fits", "swin_cl_200_test")
+
+log_path = os.path.join(project_root, "model_output", "evaluation_logs")
+
+summary_dir = os.path.join(
+    project_root, "model_output", "summary_output_folder", "swin_cl_200_test"
+)
+
+os.makedirs(summary_dir, exist_ok=True)
+os.makedirs(model_dir, exist_ok=True)
+os.makedirs(loss_dir, exist_ok=True)
+os.makedirs(output_dir, exist_ok=True)
+os.makedirs(log_dir, exist_ok=True)
+
+use_input_guidance = True
